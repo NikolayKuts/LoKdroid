0d49d3975ab2f5a205fa05982d8901a46b2688cf
NikolayGini <nikolayk@gini-apps.com>
2024-07-24 14:50:45 +0300

    Implement retrieving library version in publishing plugin from version catalog

--- buildSrc/src/main/kotlin/MavenLocalPublishingPlugin.kt
+++ buildSrc/src/main/kotlin/MavenLocalPublishingPlugin.kt
@@ -1,9 +1,11 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByType
 
 class MavenLocalPublishingPlugin : Plugin<Project> {
 
@@ -12,12 +14,20 @@ class MavenLocalPublishingPlugin : Plugin<Project> {
             extensions.configure<PublishingExtension>("publishing") {
                 publications {
                     create<MavenPublication>("release") {
+                        val libraryVersion = target.rootProject
+                            .extensions
+                            .getByType<VersionCatalogsExtension>()
+                            .named("libs")
+                            .findVersion("lokdroid")
+                            .get()
+                            .displayName
+
                         // Utilizing components.release requires configuring software components
                         from(components["release"])
 
                         groupId = "com.lokdroid"
                         artifactId = "core"
-                        version = "0.0.1"
+                        version = libraryVersion
                     }
 
                     repositories {
